5f911d2b3a3072e39096319bc67a211948c62eaf
Wisdom <wisdomngozi59@gmail.com>
2026-08-16 09:42:51 -0700

    completely purge node_modules from git tracking

--- backend/server.js
+++ backend/server.js
@@ -363,6 +363,6 @@ wss.on('connection', (ws) => {
 });
 
 const PORT = 4000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0',() => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
